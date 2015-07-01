9024d72b4226cfbf65c6cbdd66685710f1742abc
Jorge Velazquez <jorgevc@fcfm.buap.mx>
2015-06-30 19:48:14 -0500

    Cleaned unused files

--- ControlDinamico.h
+++
@@ -1,7 +0,0 @@
-
-#include "InterfaseControlDinamico.h"
-
-int* activa_escucha(int clave);
-void cierra_escucha(void);
-void Servicio(int T,char *contenedor,Float2D_MP *MP_RhoVsT, Float2D_MP *MP_RhoVsT_1, Dist_MP *MP_RhoDist, Dist_MP *MP_RhoDist_1, estado *e, int MaxPar);
-void SalidaCD(int *i,int T_max);
